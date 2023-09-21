718aca1f28733164355a63cb4aad11351fc32cbe
AlexisTech41 <solucionestechmx@gmail.com>
2023-09-20 21:57:24 -0600

    Se arregla titulo de navbar

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">Soluciones Tech MX by Fox</a>
+                <a class="navbar-brand" href="#">Soluciones Tech MX by Alexis Tech</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
